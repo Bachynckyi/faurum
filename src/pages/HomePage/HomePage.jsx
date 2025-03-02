7c384fdbdcd96d0be7d42422587fde46d4c3f048
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2025-03-02 21:36:04 +0200

    update_02/03/2025_test4

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -46,6 +46,21 @@ const HomePage = () => {
       }
   }, [i18n.language]);
 
+  const logoMap = {
+    en: "../../images/Logo_en.png",
+    de: "../../images/Logo_de.png",
+    ua: "../../images/Logo_ua.png",
+  };
+  const currentLogo = logoMap[i18n.language] || logoMap.ua;
+
+  const siteNameMap = {
+    en: "AURUM",
+    de: "AURUM",
+    ua: "АУРУМ",
+  };
+  
+  const currentSiteName = siteNameMap[i18n.language] || siteNameMap.ua;
+
   return (
     <>  
         <Helmet>
@@ -53,6 +68,11 @@ const HomePage = () => {
             <meta name="description" content={t("Home_meta_description")}/>
             <meta property="og:title" content={t("Home_meta_title")}/>
             <meta property="og:description" content={t("Home_meta_description")}/>
+            <meta property="og:image" content={currentLogo}/>
+            <meta property="og:image:width" content="1200"/>
+            <meta property="og:image:height" content="630"/>
+            <meta property="og:site_name" content={currentSiteName}/>
+            <meta property="og:type" content="website"/> 
         </Helmet>
         <div className={scss.container}>
             <div className={scss.background_wrapper1}></div>
