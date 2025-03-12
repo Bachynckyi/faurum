8e8f7891991c2a830f8bdbfce5f7735608736e02
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2025-03-12 22:21:07 +0200

    update_12/03/2025-6

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -8,22 +8,34 @@ import { de } from "./translations.js/de";
 const resources = {
   en: { translation: en },
   ua: { translation: ua },
-  ru: { translation: ua },
+  ru: { translation: ua }, 
   de: { translation: de },
 };
 
 const isPrerender = navigator.userAgent === "ReactSnap";
+let browserLang = navigator.language || navigator.userLanguage;
+if (browserLang.includes("-")) {
+  browserLang = browserLang.split("-")[0]; 
+}
+if (browserLang === "ru") {
+  browserLang = "ua"; 
+}
+if (!["en", "ua", "de"].includes(browserLang)) {
+  browserLang = "en"; 
+}
+
+localStorage.setItem("i18nextLng", browserLang);
 
 i18n
-  .use(initReactI18next) 
+  .use(initReactI18next)
   .use(LanguageDetector)
   .init({
     resources,
-    fallbackLng: "ua",
+    fallbackLng: "en",
     supportedLngs: ["en", "ua", "de"],
     nonExplicitSupportedLngs: true,
     detection: {
-      order: isPrerender ? ["path"] : ["querystring", "localStorage", "navigator", "htmlTag"], 
+      order: isPrerender ? ["path"] : ["localStorage", "querystring", "navigator", "htmlTag"],
       lookupQuerystring: "lng",
       lookupLocalStorage: "i18nextLng",
       caches: ["localStorage"],
@@ -34,9 +46,7 @@ i18n
   .then(() => {
     const currentLang = i18n.language;
     if (currentLang.includes("-")) {
-      const shortLang = currentLang.split("-")[0];
-      i18n.changeLanguage(shortLang);
-      localStorage.setItem("i18nextLng", shortLang);
+      i18n.changeLanguage(currentLang.split("-")[0]);
     }
   });
 
