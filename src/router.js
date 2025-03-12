028c76e5b7031874d4cbde7c82b84e14b3b1d988
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2025-03-12 08:45:32 +0200

    update_12/03/2025-1

--- src/router.js
+++ src/router.js
@@ -218,7 +218,7 @@ const UserRoutes = () => {
             </Routes>
           </div>
         ) : (
-          <AnimatePresence mode='wait'>
+          <AnimatePresence mode='sync'>
             <motion.div
               key={location.pathname}
               variants={animationOptions}
